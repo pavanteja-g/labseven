c4335cdd4c64ab1a2f10239c5a334d490108b31e
Pavanteja-g <gpavanteja.gpt@gmail.com>
2024-03-01 08:31:25 +0530

    passed for 15

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,7 +13,9 @@ public class App
     }
 
     public static String fizzBuzz(int n) {
-
+        if(n%3==0 && n%5==0){
+            return "FizzBuzz";
+        }
         if (n % 3 == 0) {
             return "Fizz";
         }
